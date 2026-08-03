a5b672a1f65a7af47a927ec0b51ddfa6cd8a806d
Akshit Gupta <145148177+Akshitguptaji@users.noreply.github.com>
2026-08-03 23:34:28 +0530

    creates the router for finder

--- backend/src/routes/main_routes.ts
+++ backend/src/routes/main_routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import itemroutes from "../routes/itemroutes.js";
+import publicroutes from "../routes/public_routes.js";
 // import commentroutes from "./commentRoutes.js";
 const router = Router();
 // router.use("/api/post", Postroutes);
 router.use("/api/items", itemroutes);
-
+router.use("api/submitreport", publicroutes);
 export default router;
--- backend/src/routes/public_routes.ts
+++ backend/src/routes/public_routes.ts
@@ -1,1 +1,16 @@
 import { Router } from "express";
+import { rateLimiter } from "../middleware/ratelimit.js";
+
+import { verifyTurnstile } from "../middleware/turnstile.js";
+import { submitFoundReport } from "../controllers/public.contoller.js";
+
+const publicroutes = Router();
+
+publicroutes.post(
+  "/:shortCode/submitreport",
+  verifyTurnstile,
+  rateLimiter,
+  submitFoundReport,
+);
+
+export default publicroutes;
