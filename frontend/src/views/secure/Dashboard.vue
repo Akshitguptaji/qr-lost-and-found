0478a7972baf3cadb6dc3dc061e4d0be092da036
Akshit Gupta <145148177+Akshitguptaji@users.noreply.github.com>
2026-08-17 00:45:09 +0530

    day 5 doing client project

--- frontend/src/views/secure/Dashboard.vue
+++ frontend/src/views/secure/Dashboard.vue
@@ -27,7 +27,7 @@ const handleLogout = async () => {
     await auth.signOut();
     router.push("/login");
   } catch (error) {
-    console.error("logout of error:", error);
+    console.error("logout because of error:", error);
   }
 };
 </script>
